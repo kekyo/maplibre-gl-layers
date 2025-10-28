4b25cba591e0a333307cdbf36fe9a0a9be107466
Kouji Matsui <k@kekyo.net>
2025-10-28 11:15:24 +0900

    feat: Added demo page deployment script.

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import prettierMax from 'prettier-max';
+import screwUp from 'screw-up';
 
 export default defineConfig({
+  base: '/maplibre-gl-layers/',
   plugins: [
     prettierMax(), // Use default settings
+    screwUp({
+      outputMetadataFile: true,
+    }),
   ],
 });
