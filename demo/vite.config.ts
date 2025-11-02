c8fbc3c2da870151716fd37d1d5ce2bdda5b8bfb
Kouji Matsui <k@kekyo.net>
2025-11-02 10:58:33 +0900

    feat: Improved HMR in demo project.

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from 'vite';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import prettierMax from 'prettier-max';
 import screwUp from 'screw-up';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   base: '/maplibre-gl-layers/',
+  resolve: {
+    alias: {
+      // Switch dependency libraries to source references to make HMR and debugging easier
+      'maplibre-gl-layers': resolve(__dirname, '../maplibre-gl-layers/src'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['maplibre-gl-layers'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['maplibre-gl-layers'],
+    },
+  },
   plugins: [
     prettierMax(), // Use default settings
     screwUp({
